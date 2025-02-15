755a7ebacccfcb26a19e680443eec9b144734a45
Mariya22M <lutsakmaria14@gmail.com>
2025-02-15 15:01:26 +0200

    Add ErrorMessage component and its styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-
+import App from "./App"; 
 import "./index.css";
-
 
 const rootElement = document.getElementById("root") as HTMLElement;
 
 if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement); 
-  root.render; 
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
 }
